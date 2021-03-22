a08482ea4a9904a2cc1cf83b808c99192d3137ff
duryb <brennandury@gmail.com>
2021-03-22 02:09:28 -0700

    Added FileReader rev test

--- src/main/FileReader.java
+++ src/main/FileReader.java
@@ -10,11 +10,14 @@ import java.util.*;
 public final class FileReader {
     private static final File[] texts = new File("./src/resources/texts").listFiles();
     private static final File[] revTexts = new File("./src/resources/rev-texts").listFiles();
+    private static final File[] testRevTexts = new File("./src/resources/test-rev-texts").listFiles();
+
 
     private static final Map<String, Set<String>> revTags = new HashMap<String, Set<String>>(); //folder name to final name
     static {
         revTags.put("Trump2020", new HashSet<String>(Arrays.asList("Donald Trump:", "Donald J. Trump:", "President Trump:"))); //relies on an inconsistent rule of the text files to notate who is talking
         revTags.put("Biden2020", new HashSet<String>(Arrays.asList("Joe Biden:", "Joe Biden :"))); //relies on an inconsistent rule of the text files to notate who is talking
+        revTags.put("test-rev-president", new HashSet<String>(Arrays.asList("President:", "President :")));
     }
 
     /**
@@ -39,7 +42,7 @@ public final class FileReader {
         }
     }
 
-    private static Set<String> readTextsFile(File speechFile) throws FileNotFoundException {
+    public static Set<String> readTextsFile(File speechFile) throws FileNotFoundException {
         Set<String> linesBySpeaker = new HashSet<String>();
         Set<String> tags = createUpperCaseTags(speechFile.getParentFile().getName());
         Scanner sc = new Scanner(speechFile);
@@ -78,7 +81,7 @@ public final class FileReader {
         return tags;
     }
 
-    private static Set<String> readRevTextsFile(File speechFile) throws FileNotFoundException {
+    public static Set<String> readRevTextsFile(File speechFile) throws FileNotFoundException {
         Set<String> linesBySpeaker = new HashSet<String>();
         Set<String> tags = revTags.get(speechFile.getParentFile().getName());
         boolean speaker = false;
